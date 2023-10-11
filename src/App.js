a245f1893f7ccda76fa6ac99b83abf46844c3bb5
jamaneffa <jamaneffa@yahoo.com>
2023-10-11 11:02:46 -0300

    update styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './assets/styles/App.css';
+import './assets/styles/app.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Dashboard from './components/main/Dashboard';
